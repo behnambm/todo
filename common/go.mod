04ec3115e0f1a28f792a44fd092ba09cd833fa91
Behnam Mohammadzadeh <behnam.mohamadzadeh21@gmail.com>
2023-07-24 22:05:33 +0330

    add common package

---
+++ common/go.mod
@@ -0,0 +1,3 @@
+module github.com/behnambm/todo/common
+
+go 1.20
---
+++ common/types/brokertypes/types.go
@@ -0,0 +1,30 @@
+package brokertypes
+
+const (
+	MessageTypeUserRegister = "mt_us_r"
+	MessageTypeTodoCreate   = "mt_td_c"
+	MessageTypeTodoUpdate   = "mt_td_u"
+	MessageTypeItemCreate   = "mt_it_c"
+	MessageTypeItemUpdate   = "mt_it_u"
+)
+
+type UserMessage struct {
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type TodoMessage struct {
+	ID          int    `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	UserId      int    `json:"userId"`
+}
+
+type ItemMessage struct {
+	ID       int    `json:"id"`
+	Title    string `json:"title"`
+	Priority int    `json:"priority"`
+	UserId   int    `json:"userId"`
+	TodoId   int    `json:"todoId"`
+}

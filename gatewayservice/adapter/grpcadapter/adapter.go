094c27e804945094e336861fba86986a92138d52
Behnam Mohammadzadeh <behnam.mohamadzadeh21@gmail.com>
2023-07-26 18:27:36 +0330

    add `adapter` to gateway service

---
+++ gatewayservice/adapter/grpcadapter/adapter.go
@@ -0,0 +1,161 @@
+package grpcadapter
+
+import (
+	"context"
+	"fmt"
+	"github.com/behnambm/todo/gatewayservice/protobuf/authproto"
+	"github.com/behnambm/todo/gatewayservice/protobuf/todoproto"
+	"github.com/behnambm/todo/gatewayservice/protobuf/userproto"
+	"github.com/behnambm/todo/gatewayservice/types"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"log"
+)
+
+type GRPCAdapter struct {
+	authClient authproto.AuthClient
+	userClient userproto.UserClient
+	todoClient todoproto.TodoClient
+}
+
+func New(UserGRPCServiceURL, AuthGRPCServiceURL, TodoGRPCServiceURL string) GRPCAdapter {
+	userConn, err := grpc.Dial(UserGRPCServiceURL, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("user gRPC connection error %v \n", err)
+	}
+	userClient := userproto.NewUserClient(userConn)
+
+	authConn, err := grpc.Dial(AuthGRPCServiceURL, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("auth gRPC connection error %v \n", err)
+	}
+	authClient := authproto.NewAuthClient(authConn)
+
+	todoConn, err := grpc.Dial(TodoGRPCServiceURL, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("todo gRPC connection error %v \n", err)
+	}
+	todoClient := todoproto.NewTodoClient(todoConn)
+
+	return GRPCAdapter{
+		userClient: userClient,
+		authClient: authClient,
+		todoClient: todoClient,
+	}
+}
+
+func (g GRPCAdapter) GetUserByEmail(email string) (types.User, error) {
+	user, err := g.userClient.GetUserByEmail(context.Background(), &userproto.GetUserByEmailRequest{Email: email})
+	if err != nil {
+		return types.User{}, fmt.Errorf("[gRPC ADAPTER] GetUserByEmail - %w", err)
+	}
+
+	log.Println("i got the user from gRPC : ", user)
+
+	return types.User{
+		Name:     user.GetName(),
+		Email:    user.GetEmail(),
+		Password: user.GetPassword(),
+		ID:       user.GetId(),
+	}, nil
+}
+
+func (g GRPCAdapter) GetToken(userId int64) (string, error) {
+	token, err := g.authClient.GetToken(
+		context.Background(), &authproto.GetTokenRequest{Userid: userId},
+	)
+	if err != nil {
+		return "", fmt.Errorf("[gRPC ADAPTER] GetToken - %w", err)
+	}
+
+	return token.Token, nil
+}
+
+func (g GRPCAdapter) IsValidWithClaim(token string) (map[string]string, bool) {
+	reply, err := g.authClient.ValidateTokenWithClaims(context.Background(), &authproto.ValidateTokenWithClaimsRequest{
+		Token: token,
+	})
+	if err != nil {
+		return nil, false
+	}
+
+	return reply.Claims, reply.IsValid
+}
+
+func (g GRPCAdapter) GetUserByID(userId int64) (types.User, error) {
+	user, err := g.userClient.GetUserById(context.Background(), &userproto.GetUserByIdRequest{UserId: userId})
+	if err != nil {
+		return types.User{}, fmt.Errorf("[gRPC] GetUserByID - %w", err)
+	}
+
+	return types.User{
+		Name:     user.GetName(),
+		Email:    user.GetEmail(),
+		Password: user.GetPassword(),
+		ID:       user.GetId(),
+	}, nil
+}
+
+func (g GRPCAdapter) GetUserTodosWithItems(userId int64) ([]types.TodoWithItems, error) {
+	todosReply, err := g.todoClient.GetUserTodosWithItems(
+		context.Background(), &todoproto.UserTodosRequest{UserId: userId},
+	)
+	if err != nil {
+		return nil, fmt.Errorf("[gRPC] GetUserTodosWithItems - %w", err)
+	}
+
+	var todosWithItems []types.TodoWithItems
+
+	for _, todoReply := range todosReply.Todos {
+		var items []types.Item
+
+		for _, itemReply := range todoReply.Items {
+			items = append(items, types.Item{
+				ID:       itemReply.GetId(),
+				Title:    itemReply.GetTitle(),
+				Priority: int(itemReply.GetPriority()),
+			})
+		}
+
+		todosWithItems = append(todosWithItems, types.TodoWithItems{
+			Todo: types.Todo{
+				ID:          todoReply.GetId(),
+				Name:        todoReply.GetName(),
+				Description: todoReply.GetDescription(),
+				UserId:      todoReply.GetUserId(),
+			},
+			Items: items,
+		})
+	}
+
+	return todosWithItems, nil
+}
+
+func (g GRPCAdapter) GetTodo(todoId int64) (types.Todo, error) {
+	todoReply, err := g.todoClient.GetTodo(context.Background(), &todoproto.TodoRequest{TodoId: todoId})
+	if err != nil {
+		return types.Todo{}, fmt.Errorf("[gRPC] GetTodo - %w", err)
+	}
+
+	return types.Todo{
+		ID:          todoReply.GetId(),
+		Name:        todoReply.GetName(),
+		Description: todoReply.GetDescription(),
+		UserId:      todoReply.GetUserId(),
+	}, nil
+}
+
+func (g GRPCAdapter) GetItem(itemId int64) (types.Item, error) {
+	reply, err := g.todoClient.GetItem(context.Background(), &todoproto.ItemRequest{ItemId: itemId})
+	if err != nil {
+		return types.Item{}, fmt.Errorf("[gRPC] GetItem - %w", err)
+	}
+
+	return types.Item{
+		ID:       reply.GetId(),
+		Title:    reply.GetTitle(),
+		Priority: int(reply.GetPriority()),
+		UserId:   reply.GetUserId(),
+		TodoId:   reply.GetTodoId(),
+	}, nil
+}
---
+++ gatewayservice/adapter/rabbitmqadapter/adapter.go
@@ -0,0 +1,252 @@
+package rabbitmqadapter
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/behnambm/todo/common/types/brokertypes"
+	"github.com/behnambm/todo/gatewayservice/types"
+	"github.com/streadway/amqp"
+	"log"
+)
+
+type RabbitMQAdapter struct {
+	conn          *amqp.Connection
+	userQueueName string
+	todoQueueName string
+}
+
+func New(amqpServerURI, userQueueName, todoQueueName string) RabbitMQAdapter {
+	connectRabbitMQ, err := amqp.Dial(amqpServerURI)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	return RabbitMQAdapter{
+		conn:          connectRabbitMQ,
+		userQueueName: userQueueName,
+		todoQueueName: todoQueueName,
+	}
+}
+
+func (r RabbitMQAdapter) CreateUser(user types.User) error {
+	channelRabbitMQ, err := r.conn.Channel()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer channelRabbitMQ.Close()
+
+	msg := brokertypes.UserMessage{
+		Name:     user.Name,
+		Email:    user.Email,
+		Password: user.Password,
+	}
+	msgBody, jsonErr := json.Marshal(msg)
+	if jsonErr != nil {
+		log.Println("[Broker Adapter] CreateUser - error marshaling to json: ", jsonErr)
+
+		return fmt.Errorf("[Broker Adapter] CreateUser - error marshaling to json - %w", err)
+	}
+	message := amqp.Publishing{
+		ContentType: "application/json",
+		Body:        msgBody,
+		Type:        brokertypes.MessageTypeUserRegister,
+	}
+
+	publishErr := channelRabbitMQ.Publish("", r.userQueueName, false, false, message)
+	if publishErr != nil {
+		return fmt.Errorf("[Broker Adapter] CreateUser - %w", err)
+	}
+
+	return nil
+}
+
+func (r RabbitMQAdapter) CreateTodo(todo types.Todo) error {
+	channelRabbitMQ, err := r.conn.Channel()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer channelRabbitMQ.Close()
+
+	msg := brokertypes.TodoMessage{
+		Name:        todo.Name,
+		Description: todo.Description,
+		UserId:      todo.UserId,
+	}
+
+	msgBody, jsonErr := json.Marshal(msg)
+	if jsonErr != nil {
+		log.Println("[Broker Adapter] CreateTodo - error marshaling to json: ", jsonErr)
+
+		return fmt.Errorf("[Broker Adapter] CreateTodo - error marshaling to json - %w", err)
+	}
+	message := amqp.Publishing{
+		ContentType: "application/json",
+		Body:        msgBody,
+		Type:        brokertypes.MessageTypeTodoCreate,
+	}
+
+	publishErr := channelRabbitMQ.Publish("", r.todoQueueName, false, false, message)
+	if publishErr != nil {
+		return fmt.Errorf("[Broker Adapter] CreateTodo - %w", err)
+	}
+
+	return nil
+
+}
+
+func (r RabbitMQAdapter) UpdateTodo(todo types.Todo) error {
+	channelRabbitMQ, err := r.conn.Channel()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer channelRabbitMQ.Close()
+
+	msg := brokertypes.TodoMessage{
+		ID:          todo.ID,
+		Name:        todo.Name,
+		Description: todo.Description,
+	}
+
+	msgBody, jsonErr := json.Marshal(msg)
+	if jsonErr != nil {
+		return fmt.Errorf("[Broker Adapter] UpdateTodo - error marshaling to json - %w", err)
+	}
+	message := amqp.Publishing{
+		ContentType: "application/json",
+		Body:        msgBody,
+		Type:        brokertypes.MessageTypeTodoUpdate,
+	}
+
+	publishErr := channelRabbitMQ.Publish("", r.todoQueueName, false, false, message)
+	if publishErr != nil {
+		return fmt.Errorf("[Broker Adapter] UpdateTodo - %w", err)
+	}
+
+	return nil
+
+}
+
+func (r RabbitMQAdapter) DeleteTodo(todoId int64) error {
+	channelRabbitMQ, err := r.conn.Channel()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer channelRabbitMQ.Close()
+
+	msg := brokertypes.TodoMessage{ID: todoId}
+
+	msgBody, jsonErr := json.Marshal(msg)
+	if jsonErr != nil {
+		return fmt.Errorf("[Broker Adapter] DeleteTodo - error marshaling to json - %w", err)
+	}
+	message := amqp.Publishing{
+		ContentType: "application/json",
+		Body:        msgBody,
+		Type:        brokertypes.MessageTypeTodoDelete,
+	}
+
+	publishErr := channelRabbitMQ.Publish("", r.todoQueueName, false, false, message)
+	if publishErr != nil {
+		return fmt.Errorf("[Broker Adapter] DeleteTodo - %w", err)
+	}
+
+	return nil
+
+}
+
+func (r RabbitMQAdapter) CreateItem(item types.Item) error {
+	channelRabbitMQ, err := r.conn.Channel()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer channelRabbitMQ.Close()
+
+	msg := brokertypes.ItemMessage{
+		Title:    item.Title,
+		Priority: item.Priority,
+		TodoId:   item.TodoId,
+		UserId:   item.UserId,
+	}
+
+	msgBody, jsonErr := json.Marshal(msg)
+	if jsonErr != nil {
+		log.Println("[Broker Adapter] CreateItem - error marshaling to json: ", jsonErr)
+
+		return fmt.Errorf("[Broker Adapter] CreateItem - error marshaling to json - %w", err)
+	}
+	message := amqp.Publishing{
+		ContentType: "application/json",
+		Body:        msgBody,
+		Type:        brokertypes.MessageTypeItemCreate,
+	}
+
+	publishErr := channelRabbitMQ.Publish("", r.todoQueueName, false, false, message)
+	if publishErr != nil {
+		return fmt.Errorf("[Broker Adapter] CreateItem - %w", err)
+	}
+
+	return nil
+
+}
+
+func (r RabbitMQAdapter) UpdateItem(item types.Item) error {
+	channelRabbitMQ, err := r.conn.Channel()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer channelRabbitMQ.Close()
+
+	msg := brokertypes.ItemMessage{
+		ID:       item.ID,
+		Title:    item.Title,
+		Priority: item.Priority,
+	}
+
+	msgBody, jsonErr := json.Marshal(msg)
+	if jsonErr != nil {
+		log.Println("[Broker Adapter] UpdateItem - error marshaling to json: ", jsonErr)
+
+		return fmt.Errorf("[Broker Adapter] UpdateItem - error marshaling to json - %w", err)
+	}
+	message := amqp.Publishing{
+		ContentType: "application/json",
+		Body:        msgBody,
+		Type:        brokertypes.MessageTypeItemUpdate,
+	}
+
+	publishErr := channelRabbitMQ.Publish("", r.todoQueueName, false, false, message)
+	if publishErr != nil {
+		return fmt.Errorf("[Broker Adapter] UpdateItem - %w", err)
+	}
+
+	return nil
+}
+
+func (r RabbitMQAdapter) DeleteItem(itemId int64) error {
+	channelRabbitMQ, err := r.conn.Channel()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer channelRabbitMQ.Close()
+
+	msg := brokertypes.ItemMessage{ID: itemId}
+
+	msgBody, jsonErr := json.Marshal(msg)
+	if jsonErr != nil {
+		log.Println("[Broker Adapter] DeleteItem - error marshaling to json: ", jsonErr)
+
+		return fmt.Errorf("[Broker Adapter] DeleteItem - error marshaling to json - %w", err)
+	}
+	message := amqp.Publishing{
+		ContentType: "application/json",
+		Body:        msgBody,
+		Type:        brokertypes.MessageTypeItemDelete,
+	}
+
+	publishErr := channelRabbitMQ.Publish("", r.todoQueueName, false, false, message)
+	if publishErr != nil {
+		return fmt.Errorf("[Broker Adapter] DeleteItem - %w", err)
+	}
+
+	return nil
+}

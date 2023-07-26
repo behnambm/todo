3af85c852c0759866415cc1dc9f34012d1514f12
Behnam Mohammadzadeh <behnam.mohamadzadeh21@gmail.com>
2023-07-26 18:30:09 +0330

    add `types` to gateway service

---
+++ gatewayservice/types/main.go
@@ -0,0 +1,28 @@
+package types
+
+type User struct {
+	ID       int64
+	Name     string
+	Email    string
+	Password string
+}
+
+type Todo struct {
+	ID          int64
+	Name        string
+	Description string
+	UserId      int64
+}
+
+type Item struct {
+	ID       int64
+	Title    string
+	Priority int
+	UserId   int64
+	TodoId   int64
+}
+
+type TodoWithItems struct {
+	Todo
+	Items []Item
+}

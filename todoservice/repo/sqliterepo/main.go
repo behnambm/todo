c3b605c7d046f46d73124c3f7c82047a6bab1b27
Behnam Mohammadzadeh <behnam.mohamadzadeh21@gmail.com>
2023-07-26 18:16:50 +0330

    add `DeleteTodo` & `DeleteItem` to todo service repo

--- todoservice/repo/sqliterepo/main.go
+++ todoservice/repo/sqliterepo/main.go
@@ -14,11 +14,11 @@ type Repo struct {
 func New(dsn string) *Repo {
 	conn, err := sql.Open("sqlite3", dsn)
 	if err != nil {
-		panic(err)
+		log.Fatalln(err)
 	}
 
 	if pingErr := conn.Ping(); pingErr != nil {
-		panic(pingErr)
+		log.Fatalln(pingErr)
 	}
 
 	return &Repo{
@@ -26,8 +26,8 @@ func New(dsn string) *Repo {
 	}
 }
 
-func (r Repo) GetTodoById(id int) (types.Todo, error) {
-	row := r.db.QueryRow(`SELECT id, name, description, user_id FROM todo WHERE id = ?`, id)
+func (r Repo) GetTodoById(todoId int64) (types.Todo, error) {
+	row := r.db.QueryRow(`SELECT id, name, description, user_id FROM todo WHERE id = ?`, todoId)
 	if row.Err() != nil {
 		return types.Todo{}, row.Err()
 	}
@@ -40,8 +40,8 @@ func (r Repo) GetTodoById(id int) (types.Todo, error) {
 	return todo, nil
 }
 
-func (r Repo) GetUserTodos(userId int) ([]types.Todo, error) {
-	rows, err := r.db.Query(`SELECT id, name, description FROM todo WHERE user_id = ?`, userId)
+func (r Repo) GetUserTodos(userId int64) ([]types.Todo, error) {
+	rows, err := r.db.Query(`SELECT id, name, description, user_id FROM todo WHERE user_id = ?`, userId)
 	if err != nil {
 		return nil, err
 	}
@@ -51,7 +51,7 @@ func (r Repo) GetUserTodos(userId int) ([]types.Todo, error) {
 
 	for rows.Next() {
 		todo := types.Todo{}
-		if err := rows.Scan(&todo.ID, &todo.Name, &todo.Description); err != nil {
+		if err := rows.Scan(&todo.ID, &todo.Name, &todo.Description, &todo.UserId); err != nil {
 			continue
 		}
 		todos = append(todos, todo)
@@ -60,7 +60,7 @@ func (r Repo) GetUserTodos(userId int) ([]types.Todo, error) {
 	return todos, nil
 }
 
-func (r Repo) GetTodoItems(todoId int) ([]types.Item, error) {
+func (r Repo) GetTodoItems(todoId int64) ([]types.Item, error) {
 	rows, err := r.db.Query(`SELECT id, title, priority FROM item WHERE todo_id = ?`, todoId)
 	if err != nil {
 		return nil, err
@@ -80,7 +80,7 @@ func (r Repo) GetTodoItems(todoId int) ([]types.Item, error) {
 	return items, nil
 }
 
-func (r Repo) GetUserTodosWithItems(userId int) ([]types.TodoWithItems, error) {
+func (r Repo) GetUserTodosWithItems(userId int64) ([]types.TodoWithItems, error) {
 	var todosWithItems []types.TodoWithItems
 
 	userTodos, err := r.GetUserTodos(userId)
@@ -91,7 +91,6 @@ func (r Repo) GetUserTodosWithItems(userId int) ([]types.TodoWithItems, error) {
 	for _, todo := range userTodos {
 		items, itemErr := r.GetTodoItems(todo.ID)
 		if itemErr != nil {
-			log.Println("[Repo] GetUserTodosWithItems - error getting items of ", todo.ID)
 			continue
 		}
 		todoWithItems := types.TodoWithItems{
@@ -104,7 +103,7 @@ func (r Repo) GetUserTodosWithItems(userId int) ([]types.TodoWithItems, error) {
 	return todosWithItems, nil
 }
 
-func (r Repo) GetTodoWithItems(todoId int) (types.TodoWithItems, error) {
+func (r Repo) GetTodoWithItems(todoId int64) (types.TodoWithItems, error) {
 	todo, err := r.GetTodoById(todoId)
 	if err != nil {
 		return types.TodoWithItems{}, err
@@ -136,7 +135,7 @@ func (r Repo) CreateTodo(todo types.Todo) (types.Todo, error) {
 	if idErr != nil {
 		return types.Todo{}, idErr
 	}
-	todo.ID = int(todoId)
+	todo.ID = todoId
 
 	return todo, nil
 }
@@ -153,6 +152,29 @@ func (r Repo) UpdateTodo(todo types.Todo) (types.Todo, error) {
 	return todo, nil
 }
 
+func (r Repo) DeleteTodo(todoId int64) error {
+	_, err := r.db.Exec(`DELETE FROM todo WHERE id = ?;`, todoId)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r Repo) GetItemById(itemId int64) (types.Item, error) {
+	row := r.db.QueryRow(`SELECT id, title, priority, user_id , todo_id FROM item WHERE id = ?`, itemId)
+	if row.Err() != nil {
+		return types.Item{}, row.Err()
+	}
+
+	item := types.Item{}
+	if err := row.Scan(&item.ID, &item.Title, &item.Priority, &item.UserId, &item.TodoId); err != nil {
+		return types.Item{}, err
+	}
+
+	return item, nil
+}
+
 func (r Repo) CreateItem(item types.Item) (types.Item, error) {
 	res, err := r.db.Exec(
 		`INSERT INTO item (title, priority, user_id, todo_id) VALUES (?, ?, ?, ?)`,
@@ -166,7 +188,8 @@ func (r Repo) CreateItem(item types.Item) (types.Item, error) {
 	if idErr != nil {
 		return types.Item{}, idErr
 	}
-	item.ID = int(itemId)
+
+	item.ID = itemId
 
 	return item, nil
 }
@@ -181,4 +204,13 @@ func (r Repo) UpdateItem(item types.Item) (types.Item, error) {
 	}
 
 	return item, nil
+}
+
+func (r Repo) DeleteItem(itemId int64) error {
+	_, err := r.db.Exec(`DELETE FROM item WHERE id = ?;`, itemId)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }

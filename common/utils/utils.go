f75edf8b845c66e059491b6c37d801de2a035118
Behnam Mohammadzadeh <behnam.mohamadzadeh21@gmail.com>
2023-07-26 18:27:01 +0330

    add `InterfaceToString` to common utils

---
+++ common/utils/utils.go
@@ -0,0 +1,24 @@
+package utils
+
+import "strconv"
+
+func InterfaceToString(value interface{}) string {
+	// Check if the value is already a string, and return it if it is.
+	if str, ok := value.(string); ok {
+		return str
+	}
+
+	// Check if the value is of numeric types and convert it to a string.
+	switch v := value.(type) {
+	case int:
+		return strconv.Itoa(v)
+	case int64:
+		return strconv.FormatInt(v, 10)
+	case float32:
+		return strconv.FormatFloat(float64(v), 'f', -1, 32)
+	case float64:
+		return strconv.FormatFloat(v, 'f', -1, 64)
+	}
+
+	return ""
+}

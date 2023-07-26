e9ee17e3d29cf8dea77de1f056ec2207a78f1eff
Behnam Mohammadzadeh <behnam.mohamadzadeh21@gmail.com>
2023-07-26 18:14:49 +0330

    add `GetItem` to todo service gRPC & fix some issues

--- todoservice/server/grpcserver/server.go
+++ todoservice/server/grpcserver/server.go
@@ -3,21 +3,21 @@ package grpcserver
 import (
 	"context"
 	"fmt"
+	pb "github.com/behnambm/todo/todoservice/protobuf"
 	"github.com/behnambm/todo/todoservice/types"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	"log"
 	"net"
-	"strconv"
-
-	pb "github.com/behnambm/todo/todoservice/protobuf"
 )
 
 type TodoService interface {
-	GetUserTodos(int) ([]types.Todo, error)
-	GetUserTodosWithItems(int) ([]types.TodoWithItems, error)
-	GetTodoWithItems(int) (types.TodoWithItems, error)
+	GetUserTodos(int64) ([]types.Todo, error)
+	GetTodo(int64) (types.Todo, error)
+	GetUserTodosWithItems(int64) ([]types.TodoWithItems, error)
+	GetTodoWithItems(int64) (types.TodoWithItems, error)
+	GetItem(int64) (types.Item, error)
 }
 
 // Server is used to implement User RPC service
@@ -60,13 +60,7 @@ func (s Server) Run(ctx context.Context) {
 }
 
 func (s Server) GetUserTodos(ctx context.Context, in *pb.UserTodosRequest) (*pb.UserTodosReply, error) {
-	userId, err := strconv.Atoi(in.GetUserId())
-	if err != nil {
-		log.Printf("[gRPC] GetUserTodos - userId is not valid %s \n", in.GetUserId())
-
-		return &pb.UserTodosReply{}, status.Error(codes.InvalidArgument, "userId is not valid")
-	}
-
+	userId := in.GetUserId()
 	userTodos, getTodoErr := s.todoSvc.GetUserTodos(userId)
 	if getTodoErr != nil {
 		log.Println("[gRPC] GetUserTodos - ", getTodoErr)
@@ -74,10 +68,10 @@ func (s Server) GetUserTodos(ctx context.Context, in *pb.UserTodosRequest) (*pb.
 		return &pb.UserTodosReply{}, status.Error(codes.NotFound, "user not found")
 	}
 
-	var reply []*pb.TodoMessage
+	var reply []*pb.TodoReply
 
 	for _, todo := range userTodos {
-		todoMessage := &pb.TodoMessage{Id: int64(todo.ID), Name: todo.Name, Description: todo.Description}
+		todoMessage := &pb.TodoReply{Id: int64(todo.ID), Name: todo.Name, Description: todo.Description}
 		reply = append(reply, todoMessage)
 	}
 
@@ -85,13 +79,7 @@ func (s Server) GetUserTodos(ctx context.Context, in *pb.UserTodosRequest) (*pb.
 }
 
 func (s Server) GetUserTodosWithItems(ctx context.Context, in *pb.UserTodosRequest) (*pb.UserTodosWithItemsReply, error) {
-	userId, err := strconv.Atoi(in.GetUserId())
-	if err != nil {
-		log.Printf("[gRPC] GetUserTodosWithItems - userId is not valid %s \n", in.GetUserId())
-
-		return &pb.UserTodosWithItemsReply{}, status.Error(codes.InvalidArgument, "userId is not valid")
-	}
-
+	userId := in.GetUserId()
 	userTodosWithItems, getTodoErr := s.todoSvc.GetUserTodosWithItems(userId)
 	if getTodoErr != nil {
 		log.Println("[gRPC] GetUserTodosWithItems - ", getTodoErr)
@@ -99,14 +87,16 @@ func (s Server) GetUserTodosWithItems(ctx context.Context, in *pb.UserTodosReque
 		return &pb.UserTodosWithItemsReply{}, status.Error(codes.NotFound, "user not found")
 	}
 
-	var reply []*pb.TodoWithItemsMessage
+	var reply []*pb.TodoWithItemsReply
 
 	for _, todo := range userTodosWithItems {
-		todoMessage := &pb.TodoWithItemsMessage{Id: int64(todo.ID), Name: todo.Name, Description: todo.Description}
+		todoMessage := &pb.TodoWithItemsReply{
+			Id: todo.ID, Name: todo.Name, Description: todo.Description, UserId: todo.UserId,
+		}
 
-		var todoItems []*pb.ItemMessage
+		var todoItems []*pb.ItemReply
 		for _, item := range todo.Items {
-			itemMessage := &pb.ItemMessage{Id: int64(item.ID), Title: item.Title, Priority: int64(item.Priority)}
+			itemMessage := &pb.ItemReply{Id: item.ID, Title: item.Title, Priority: int64(item.Priority)}
 			todoItems = append(todoItems, itemMessage)
 		}
 		todoMessage.Items = todoItems
@@ -116,34 +106,63 @@ func (s Server) GetUserTodosWithItems(ctx context.Context, in *pb.UserTodosReque
 	return &pb.UserTodosWithItemsReply{Todos: reply}, nil
 }
 
-func (s Server) GetTodoWithItems(ctx context.Context, in *pb.TodoRequest) (*pb.TodoWithItemsMessage, error) {
-	todoId, err := strconv.Atoi(in.GetTodoId())
-	if err != nil {
-		log.Printf("[gRPC] GetTodoWithItems - todoId is not valid %s \n", in.GetTodoId())
-
-		return &pb.TodoWithItemsMessage{}, status.Error(codes.InvalidArgument, "todoId is not valid")
-	}
-
+func (s Server) GetTodoWithItems(ctx context.Context, in *pb.TodoRequest) (*pb.TodoWithItemsReply, error) {
+	todoId := in.GetTodoId()
 	todoWithItems, getTodoErr := s.todoSvc.GetTodoWithItems(todoId)
 	if getTodoErr != nil {
 		log.Println("[gRPC] GetTodoWithItems - ", getTodoErr)
 
-		return &pb.TodoWithItemsMessage{}, status.Error(codes.NotFound, "todo not found")
+		return &pb.TodoWithItemsReply{}, status.Error(codes.NotFound, "todo not found")
 	}
 
-	todoWithItemsMessage := &pb.TodoWithItemsMessage{
-		Id:          int64(todoWithItems.ID),
+	todoWithItemsMessage := &pb.TodoWithItemsReply{
+		Id:          todoWithItems.ID,
 		Name:        todoWithItems.Name,
 		Description: todoWithItems.Description,
 	}
 
-	var itemsMessage []*pb.ItemMessage
+	var itemsMessage []*pb.ItemReply
 	for _, item := range todoWithItems.Items {
-		itemMessage := &pb.ItemMessage{Id: int64(item.ID), Title: item.Title, Priority: int64(item.Priority)}
+		itemMessage := &pb.ItemReply{Id: int64(item.ID), Title: item.Title, Priority: int64(item.Priority)}
 		itemsMessage = append(itemsMessage, itemMessage)
 	}
 
 	todoWithItemsMessage.Items = itemsMessage
 
 	return todoWithItemsMessage, nil
+}
+
+func (s Server) GetTodo(ctx context.Context, in *pb.TodoRequest) (*pb.TodoReply, error) {
+	todoId := in.GetTodoId()
+	todo, err := s.todoSvc.GetTodo(todoId)
+	if err != nil {
+		log.Println("[gRPC] GetTodo - ", err)
+
+		return &pb.TodoReply{}, status.Error(codes.NotFound, "todo not found")
+	}
+
+	return &pb.TodoReply{
+		Id:          todo.ID,
+		Name:        todo.Name,
+		Description: todo.Description,
+		UserId:      todo.UserId,
+	}, nil
+}
+
+func (s Server) GetItem(ctx context.Context, in *pb.ItemRequest) (*pb.ItemReply, error) {
+	itemId := in.GetItemId()
+	item, err := s.todoSvc.GetItem(itemId)
+	if err != nil {
+		log.Println("[gRPC] GetItem - ", err)
+
+		return &pb.ItemReply{}, status.Error(codes.NotFound, "item not found")
+	}
+
+	return &pb.ItemReply{
+		Id:       item.ID,
+		Title:    item.Title,
+		Priority: int64(item.Priority),
+		UserId:   item.UserId,
+		TodoId:   item.TodoId,
+	}, nil
 }
